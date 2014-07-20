b28687cc1cd10266eaed016000b86a1f49d37984
Kevin Cox <kevincox@kevincox.ca>
2014-07-20 14:50:15 -0400

    Initial commit.

---
+++ upload.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+api="${1:-http://localhost:8080}"
+user="${2:-1}"
+pass="${3:-passwd}"
+
+tok="$(curl -XPOST -H"Content-Type: application/json" \
+	--data-binary "{\"user\":\"$user\",\"pass\":\"$pass\"}" \
+	"$api/auth" | sed -rzne 's/.*"token"\s*:\s*"([0-9A-F]+\$[0-9A-F]+)".*/\1/p'
+)"
+
+for f in *; do
+	mime="$(file -bi "$f")"
+	echo "Uploading $f ($mime)"
+	curl -XPUT -H"Authorization: Bearer $tok" -H"Content-Type: $mime" \
+	     --data-binary "@$f" "$api/blob"
+done
