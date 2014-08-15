7fb8aff5720ccd9ca6e6536e0589be807c46e7f2
Kevin Cox <kevincox@kevincox.ca>
2014-08-14 21:38:10 -0400

    New header and footer assets.

--- upload.sh
+++ upload.sh
@@ -9,7 +9,7 @@ tok="$(curl -XPOST -H"Content-Type: application/json" \
 	"$api/auth" | sed -rzne 's/.*"token"\s*:\s*"([0-9A-F]+\$[0-9A-F]+)".*/\1/p'
 )"
 
-for f in *; do
+find -not -path '*/.*' -type f | while read f; do
 	mime="$(file -bi "$f")"
 	echo "Uploading $f ($mime)"
 	curl -XPUT -H"Authorization: Bearer $tok" -H"Content-Type: $mime" \
